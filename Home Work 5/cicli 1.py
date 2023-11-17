ca55dd194accdcdf60692ff9efa291e3c23b588b
MadiKaliyev <150113975+MadiKaliyev@users.noreply.github.com>
2023-11-17 18:32:31 +0600

    hw5
    hw5

---
+++ Home Work 5/cicli 1.py
@@ -0,0 +1,27 @@
+# Задание №1.
+# Написать программу для вывода узора по образцу, используя вложенный цикл.
+a = 0
+while a < 11:
+    if a == 1:
+        print("*")
+    elif a == 2:
+        print("* *")
+    elif a == 3:
+        print("* * *")
+    elif a == 4:
+        print("* * * *")
+    elif a == 5:
+        print("* * * * *")
+    elif a == 6:
+        print("* * * *")
+    elif a == 7:
+        print("* * *")
+    elif a == 8:
+        print("* *")
+    elif a == 9:
+        print("*")
+    a += 1
+
+
+
+
